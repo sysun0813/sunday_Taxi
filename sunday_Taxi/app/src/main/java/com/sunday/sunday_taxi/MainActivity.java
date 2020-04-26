294571d6461159f6a7af3c6933ca25f73e089724
unknown <ptjh0309@hanmail.net>
2020-04-26 16:40:31 +0900

    It is possible to access current GPS information.

--- sunday_Taxi/app/src/main/java/com/sunday/sunday_taxi/MainActivity.java
+++ sunday_Taxi/app/src/main/java/com/sunday/sunday_taxi/MainActivity.java
@@ -1,10 +1,18 @@
 package com.sunday.sunday_taxi;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
@@ -12,32 +20,75 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
+import net.daum.mf.map.api.MapPoint;
 import net.daum.mf.map.api.MapView;
 
 import java.util.Map;
 
 
 public class MainActivity extends AppCompatActivity {
+    private static final int PERMISSION_REQUEST_LOCATION = 10001;
+    private LocationManager locationManager;
+    private LocationListener locationListener;
+    private double latitude = 37.5571992;
+    private double longitude = 126.970536;
 
+    public MainActivity() {
+        this.locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                latitude=location.getLatitude();
+                longitude=location.getLongitude();
+            }
 
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
 
+            }
 
+            @Override
+            public void onProviderEnabled(String s) {
 
+            }
+
+            @Override
+            public void onProviderDisabled(String s) {
+
+            }
+        };
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        MapView mapView= new MapView(this);
-        RelativeLayout mapViewContainer =(RelativeLayout)findViewById(R.id.map_view);
-        mapViewContainer.addView(mapView);
+        this.locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 
+        // 안드로이드에서 권한 확인이 의무화 되어서 작성된 코드! 개념만 이해
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                    && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSION_REQUEST_LOCATION);
+                return;
+            }
+        }
+        this.locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 0.01f, this.locationListener);
 
+        Location loc = this.locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+        if(loc!=null) {
+            latitude=loc.getLatitude();
+            longitude=loc.getLongitude();
         }
 
+        MapView mapView= new MapView(this);
+        RelativeLayout mapViewContainer = findViewById(R.id.map_view);
+        mapView.setMapCenterPoint(MapPoint.mapPointWithGeoCoord(latitude,longitude),true);
 
+        mapViewContainer.addView(mapView);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -47,14 +98,28 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-
         int id = item.getItemId();
-
 
         if (id == R.id.action_settings) {
             return true;
         }
-
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSION_REQUEST_LOCATION:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // 권한 승인이 된 경우 다시 그리기
+                    recreate();
+                } else {
+                    // 권한 승인이 안 된 경우 종료
+                    finish();
+                }
+                break;
+            default:
+                break;
+        }
     }
 }
